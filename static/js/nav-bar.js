2aa1418eac47b415738b3ffd889101fed955ef4c
joao pedro <barbosaamaraljoaopedro@gmail.com>
2024-12-01 22:52:03 -0300

    ultimo commit antes da entrega

--- static/js/nav-bar.js
+++ static/js/nav-bar.js
@@ -1,45 +1,21 @@
 
-let atividadesbtn = document.getElementById('Atividades-btn');
-let mainContentElements = document.getElementsByClassName('main-content');
-let conversasbtn = document.getElementById('conversas-btn');
 let materiasbtn = document.getElementById('materias-btn')
 let materias1 = document.getElementById('materias1');
-let atividades = document.getElementById('Atividades');
 const perfilbtn = document.getElementById('perfil-btn');
 const perfil = document.getElementById('perfil');
 const cronogramasbtn = document.getElementById('Cronogramas-btn');
-const cronogramas = document.getElementById('CRONOGRAMAS');
-const image = document.querySelectorAll('.imagem');
-const foto = document.getElementById('foto');
 
-
-
-
-cronogramasbtn.addEventListener("click", () => {
-    // Obter todos os elementos com a classe 'main-content'
-    
-    materias1.style.display = 'none';
-    atividades.style.display = 'none';
-    perfil.style.display = 'none';
-    cronogramas.style.display = 'flex';
-
-});
 perfilbtn.addEventListener("click", () => {
 
     materias1.style.display = 'none';
-    atividades.style.display = 'none';
     perfil.style.display = 'grid';
-    cronogramas.style.display = 'none';
 
 });
-
 
 materiasbtn.addEventListener("click", () => {
    
     materias1.style.display = 'grid';
-    atividades.style.display = 'none';
     perfil.style.display = 'none';
-    cronogramas.style.display = 'none'
 
 });
 // Função para alternar a exibição da navbar
@@ -49,18 +25,6 @@ function toggleNavbar() {
     navbar.classList.toggle('show');
     mainContent.classList.toggle('shifted');
 }
-
-foto.addEventListener("change", () => {
-    if (foto.files.length > 0) {
-        const newImageSrc = URL.createObjectURL(foto.files[0]);
-        image.forEach(img => {
-            img.src = newImageSrc;
-        });
-    }
-})
-document.getElementById('image2').addEventListener("click", () => {
-    foto.click();
-});
 
 function toggleTheme() {
     const body = document.body;
